6db86e1a3cf1095281ce1021eb240ed022fdbac6
Chris Feilbach <pointerwars2025@gmail.com>
2025-07-04 09:02:34 +0000

    Final changes before pushing

--- linked_list.h
+++ linked_list.h
@@ -7,9 +7,9 @@
 
 // Some rules for Pointer Wars 2025:
 // 0. Implement all functions in linked_list.cc
-// 1. Feel free to add members to the structures, but please do not remove 
-//    any or rename any. Doing so will cause test infrastructure to fail
-//    to link against your shared library.
+// 1. Feel free to add members to the classes, but please do not remove 
+//    any. Doing so will cause test infrastructure to fail to link against 
+//    your shared library.
 // 2. Same goes for the function declarations.
 // 3. The malloc_fptr and free_fptr functions are used to allocate
 //    memory. Make use of these in your new and delete operators.
@@ -37,15 +37,24 @@ class linked_list{
     void operator delete(void * ptr);
 
     // Methods to implement.
+    // Returns TRUE on succcess, FALSE otherwise.
     //
     bool insert(size_t index, unsigned int data);
     bool insert_front(unsigned int data);
     bool insert_end(unsigned int data);
 
+    // Returns the first index where data is found
+    // in the list, SIZE_MAX otherwise.
+    //
     size_t find(unsigned int data) const;
 
+    // Returns TRUE if data at index index was able to be
+    // removed, FALSE otherwise.
+    //
     bool remove(size_t index);
 
+    // Returns the size of the linked list.
+    //
     size_t size() const;
 
     // Inner classes node and iterator.
@@ -56,6 +65,18 @@ class linked_list{
       node * next;
       unsigned int data;
     };
+
+    // Operator overloads for access to an individual
+    // element in the list. They technically offer more
+    // flexibility than level 1 of Pointer Wars in the C
+    // language in that you can modify them.
+    //
+    // Like std::vector, bounds checking isn't performed
+    // for operator[], and accessing an element out of
+    // bounds is undefined behavior.
+    //
+    unsigned int& operator[](size_t idx);
+    const unsigned int& operator[](size_t idx) const;
 
     // Static members for memory allocation and free
     // function pointers. Very C like.
@@ -69,6 +90,10 @@ class linked_list{
     // The head of the linked list.
     //
     node * head;
+
+    // If you hate this name, feel free to change it.
+    //
+    size_t ll_size;
 };
 
 #endif
